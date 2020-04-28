31596f9e362cd22ab106d4e6bdc62ed1f4c20749
BobLeTea <36767212+MCSZN@users.noreply.github.com>
2020-04-28 23:04:29 +0200

    Add files via upload

--- springer.py
+++ springer.py
@@ -1,16 +1,32 @@
 # To download all the springer books automatically
-import pandas as pd
 from subprocess import run
 from typing import Optional, List, Callable
 from multiprocessing import Pool, cpu_count
 import os
+try:
+    import pandas as pd
+except ImportError:
+    run(['pip', 'install', 'pandas', 'xlrd'])
 
-def download(link: str, title: str) -> None:
+def download(link: str, title: str, books : set) -> None:
     """
     runs a curl command on a given link
     """
-    run(["curl", link, "--ssl-no-revoke", "--output", f"{title}.pdf"])
+    if title not in books:
+        title = title.capitalize()
+        print(f"Downloading {title}")
+        run(["curl", link, "--ssl-no-revoke", "--output", f"{title}.pdf"])
     return
+
+def get_excel(file : str = "Free+English+textbooks.xlsx", directory : str = "books/") -> Optional[pd.DataFrame]:
+    try:
+        file = pd.read_excel(directory+file)
+        return file
+    except FileNotFoundError:
+        print("Downloading excel source file")
+        run(["curl", "https://resource-cms.springernature.com/springer-cms/rest/v1/content/17858272/data/v4",
+            "--ssl-no-revoke", "--output", f"{directory+file}"])
+    return pd.read_excel(directory+file)
 
 def get_interest() -> str:
     interests = input("Enter your interests: (separate words with a space)\n").lstrip().split()
@@ -25,9 +41,8 @@ def make_dir(dir_name: str = "books") -> str:
         os.mkdir(dir_name)
     return dir_name
 
-def filter_books(excel_file : str = "Free+English+textbooks.xlsx") -> Optional[List]:
+def filter_books(books : pd.DataFrame) -> Optional[List]:
     BASE = r'https://link.springer.com/content/pdf/10.1007%2F'
-    books = pd.read_excel(excel_file)
     books = books[['Book Title', 'DOI URL']]
     books_found = []
     interests = get_interest()
@@ -38,27 +53,25 @@ def filter_books(excel_file : str = "Free+English+textbooks.xlsx") -> Optional[L
         print(f"Found {len(book_urls)} books with name containing your subject of interest!")
         [print("\t",book) for book in book_titles]
         return book_urls, book_titles
-    print(f"Found 0 book with your interest. Do you want to retry ? (y/n)\n")
+    print(f"Found 0 book with your interest. Do you want to retry ? (y/n)")
     answer = input()
     if answer in ('n', 'no', 'NO','No'):
-        return None
-    return filter_books()
+        return None, None
+    return filter_books(books)
 
-def sneak_peek(excel_file : str = "Free+English+textbooks.xlsx", answer :str = "No") -> None:
-    books = pd.read_excel(excel_file)
+def sneak_peek(books : pd.DataFrame, answer :str = "No") -> None:
     ans = input("Do you want a sneak peek of what titles you can find in the db ? \n")
     if ans in ("y", "yes", "Y", "Yes"):
         print(books.sample(frac=1)["Book Title"].head())
-        return sneak_peek(answer="y")
+        return sneak_peek(books,answer="y")
     return None
 
 if __name__ == "__main__":
     book_dir = make_dir()
-    sneak_peek()
-    book_urls, book_titles = filter_books()
+    books = get_excel()
+    sneak_peek(books)
+    book_urls, book_titles = filter_books(books)
     os.chdir(book_dir)
+    already_downloaded = set(os.listdir())
     if book_urls:
-        list(map(download, book_urls, book_titles))
-
-# me likey
-# python logic math econ dynamic mechanic quant data linear game bayes robot deep neural stat program relativity equation
+        [download(url, title, already_downloaded) for url, title in zip(book_urls, book_titles)]
